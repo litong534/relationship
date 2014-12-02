944d060ac8e959b5207cb576e9b3782ed1a3eb8e
litong534 <litong534@sina.com>
2014-12-02 17:01:57 +0800

    relationship

---
+++ src/manytoone/User.java
@@ -0,0 +1,25 @@
+package manytoone;
+
+public class User {
+	private int id;
+	private String name;
+	private Group group;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Group getGroup() {
+		return group;
+	}
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+}
